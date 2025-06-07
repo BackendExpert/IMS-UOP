3b07e48240ab68983f58c5bd06a7fa508097850b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-06-07 21:20:44 +0530

    Update

--- client/src/pages/Projects/ManageProjects.jsx
+++ client/src/pages/Projects/ManageProjects.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import ProjectStatus from './ProjectStatus'
 
 const ManageProjects = () => {
     return (
         <div>
             <h1 className="font-bold text-emerald-600 text-xl">Projects</h1>
 
+            <ProjectStatus />
         </div>
     )
 }
---
+++ client/src/pages/Projects/ProjectStatus.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const ProjectStatus = () => {
+    const projectdata = [
+        {
+            id: 1,
+            name: 'Projects',
+            value: 4,
+            icon: '',
+            bgColor: '',
+        },
+        {
+            id: 2,
+            name: 'Ongoing Projects',
+            value: 4,
+            icon: '',
+            bgColor: '',
+        },
+        {
+            id: 3,
+            name: 'Complted Projects',
+            value: 4,
+            icon: '',
+            bgColor: '',
+        },
+    ]
+    return (
+        <div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 my-6">
+                {
+                    projectdata.map((project, index) => {
+                        return (
+                            <div className="relative bg-emerald-600 text-white p-6 rounded-2xl shadow-lg overflow-hidden hover:scale-[1.02] transition-transform duration-300" key={index}>
+                                <div className="absolute right-4 top-4 opacity-20 text-white text-6xl">
+                                    {/* <FaUserLock /> */}
+                                </div>
+
+                                <div className="relative z-10">
+                                    <div className="text-sm font-medium uppercase tracking-wide text-emerald-100">
+                                        Roles (User Types)
+                                    </div>
+                                    <div className="mt-2 text-3xl font-bold">55</div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default ProjectStatus
