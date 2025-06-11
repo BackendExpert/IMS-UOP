e5ff958890e5c83b63b2c3849f1ba4d628585c02
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-06-11 08:15:02 +0530

    Create Intern.js

---
+++ server/model/Intern.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const InternSchema = new mongoose.Schema({
+    userID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Role'
+    },
+
+    joinAt: {
+        type: Date,
+        default: new Date(),
+        required: true
+    },
+
+    InternshipEndAt: {
+        type: Date,
+        required: true
+    },
+
+    address: { type: String, required: true },
+    cv: { type: String, required: true },
+    dob: { type: Date, required: true },
+    github: { type: String, required: true, unique: true },
+    linkedin: { type: String, required: true, unique: true },
+    camups: { type: String, required: true },
+    course: { type: String, required: true },
+    isApprove: { type: Boolean, required: true, default: false },
+    isOneIntern: { type: Boolean, required: true, default: false },
+}, { timestamps: true });
+
+const Intern = mongoose.model('Intern', InternSchema);
+
+module.exports = Intern;
