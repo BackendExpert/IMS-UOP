f39b82461de49e10de692a056544715031860cac
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-06-14 07:33:25 +0530

    Update ReqeustLetter.jsx

--- client/src/pages/Interns/ReqeustLetter.jsx
+++ client/src/pages/Interns/ReqeustLetter.jsx
@@ -1,6 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 
 const ReqeustLetter = ({ internID }) => {
+    const [letter, setletter] = useState({
+        intern: '',
+        supervisor: '',
+        project: '',
+        duration: '',
+        salary: '',
+        startData: '',
+        endData: '',
+        letter: null,
+    })
+    
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setupdatedata((prev) => ({ ...prev, [name]: value }));
+    };
+
+    const handleFileChange = (e) => {
+        const { name, files } = e.target;
+        const file = files[0];
+        setupdatedata((prevData) => ({
+            ...prevData,
+            [name]: file,
+        }));
+    };
+
     return (
         <div>ReqeustLetter {internID}</div>
     )
